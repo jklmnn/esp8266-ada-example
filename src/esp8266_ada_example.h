7ae78edb94a4ee0ae73b606b1d85197d64eb2109
Johannes Kliemann <jk@jkliemann.de>
2020-03-01 14:50:30 +0100

    add arduino externally compiled library

---
+++ src/esp8266_ada_example.h
@@ -0,0 +1,12 @@
+
+#ifndef _ESP8266_ADA_EXAMPLE_H_
+#define _ESP8266_ADA_EXAMPLE_H_
+
+#include <ada_runtime.h>
+
+extern "C" void adainit(void);
+extern "C" void adafinal(void);
+extern "C" int main(void);
+
+
+#endif /* ifndef _ESP8266_ADA_EXAMPLE_H_ */
